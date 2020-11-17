ab5a196e34a4d14878f6b43b07c4118a63c61b1c
Manraj <manraj836@gmail.com>
2020-11-17 13:25:58 -0500

    Update app.py
    Moved models to app.py, removed import, restored hi route.

--- server/app.py
+++ server/app.py
@@ -2,10 +2,7 @@ from flask import Flask, request, jsonify
 from ariadne import graphql_sync, make_executable_schema, gql, load_schema_from_path
 # graphql playground
 from ariadne.constants import PLAYGROUND_HTML
-
 from flask_sqlalchemy import SQLAlchemy
-from models import *
-
 # resolvers
 from graph.resolvers import resolvers
 
@@ -13,9 +10,52 @@ type_def = gql(load_schema_from_path("graph/schema/"))
 SCHEMA = make_executable_schema(type_def, resolvers)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'   #The database URI that should be used for the connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///UWinRentdb.sqlite3'   #The database URI that should be used for the connection
 db = SQLAlchemy(app)
 
+# Define a user model
+class users(db.Model):
+    id = db.Collumn(db.Integer, primary_key = True)
+    name = db.Collumn(db.String(128), nullable = False)
+    type = db.Collumn(db.String(5), nullable = False)
+    email = db.Collumn(db.String(128), nullable = False, unique = True)
+    passwd = db.Collumn(db.String(192), nullable = False)
+    phone = db.Collumn(db.Integer, nullable = False, unique = True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+# Define a property model
+class properties(db.Model):
+    id = db.Collumn(db.Integer, primary_key = True)
+    landlord = db.Collumn(db.String(128), nullable = False)
+    location = db.Collumn(db.String(500), nullable = False)
+    price = db. Collumn(db.Float, nullable = False)
+
+    def __init__(self, landlord, location, price):
+        self.landlord = landlord
+        self.location = location
+        self.price = price
+
+# Define a property description model
+class p_descriptions(db.Model):
+    id = db.Collumn(db.Integer, primary_key = True)
+    pet_friendly = db.Collumn(db.Boolean, nullable = False)
+    smoke_free = db.Collumn(db.Boolean, nullable = False)
+    whole_house = db.Collumn(db.Boolean, nullable = False)
+    rooms = db.Collumn(db.Integer, nullable = False)
+    furnished = db.Collumn(db.Boolean, nullable = False)
+    utilities = db.Collumn(db.Boolean, nullable = False)
+
+    def __init__(self, pet_friendly, smoke_free, whole_house, rooms, furnished, utilities):
+        self.pet_friendly = pet_friendly
+        self.smoke_free = smoke_free
+        self.whole_house = whole_house
+        self.rooms = rooms
+        self.furnished = furnished
+        self.utilities = utilities
+        
 """
     ADD ENTRY TO DATABASE:
     usr = users("Kanye", "Kanyefor2024@gmail.com")      creates user object
@@ -44,14 +84,17 @@ def graphql_server():
     success, result = graphql_sync(
         SCHEMA,
         data,
-        context_value = request,
-        debug = app.debug
+        context_value=request,
+        debug=app.debug
     )
 
     status = 200 if success else 400
     return jsonify(result), status
 
 @app.route('/')
+def hi():
+    return 'hi'
+
 if __name__ == '__main__':
     db.create_all()         # create sql tables for data models
     app.run()
