a883385f890a0c3694ceb533c8cba7fd9d71bc37
Ryan Prairie <me@ryanprairie.com>
2020-11-20 13:55:36 -0500

    updated a bunch of stuff and started homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+import { HomePage } from './Scenes/HomePage'
 
 const querystuff = gql`
   query {
-  hello {
-    word
+    hello {
+      word
+    }
   }
-}
 `;
 
 const App = () => {
   const { loading, error, data } = useQuery(querystuff);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :\(</p>;
+  if (error) return <p>Error :\</p>;
 
   console.log(data);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          {data.hello.word}
-        </p>
-      </header>
+      <Router>
+        <Switch>
+          <Route
+            exact path="/"
+            render={ () => <HomePage/> }
+          />
+        </Switch>
+      </Router>
+      <p>
+        {data.hello.word}
+      </p>
     </div>
   );
 }
--- client/src/Providers/GQLProvider.jsx
+++ client/src/Providers/GQLProvider.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {  ApolloProvider, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
+// the `s` is important and will cause a CORS issue if you forget it
+// Ryan forgot it and spent 45 min debugging 1/10 wouldnt reccomend
 const link = new HttpLink({
   uri: 'https://localhost/s/graphql',
 })
