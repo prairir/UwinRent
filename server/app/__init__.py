397f64d204c00496e495420640d4b9de8ff0f0d5
Manraj <manraj836@gmail.com>
2020-11-20 16:24:38 -0500

    Update __init__.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:UWinRent-633710@localhost/UWinRent' # postgresql URI
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
 
 if __name__ == '__main__':
+    db.create_all()         # create sql tables for data models
     app.run()
