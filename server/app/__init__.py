06c360618a1a1a27cc090b1c808a7f2db21d3827
Manraj <manraj836@gmail.com>
2020-11-20 16:59:05 -0500

    Update __init__.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:UWinRent-633710@localhost/UWinRent' # postgresql URI
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+cors = CORS(app, resources={r"/graphql": {"origins": "*"}})
 
 from app import routes
 
 if __name__ == '__main__':
-    db.create_all()         # create sql tables for data models
     app.run()
