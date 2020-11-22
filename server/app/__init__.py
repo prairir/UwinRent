97e7ab33caf8391c3a569ae97f5e8327c8d1c350
Manraj <manraj836@gmail.com>
2020-11-22 16:51:46 -0500

    Update __init__.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 cors = CORS(app, resources={r"/graphql": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:UWinRent-633710@localhost/UWinRent' # postgresql URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
